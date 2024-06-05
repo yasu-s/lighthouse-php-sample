6b148015b9041290da588a00b0c527b5e0896732
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2024-06-05 17:27:11 +0900

    モデル追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'author_id');
+    }
 }
